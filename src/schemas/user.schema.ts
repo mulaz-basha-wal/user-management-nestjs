36e945671e80bd24a8667896cefc9a64af5cc943
Mulaz Basha <96713975+mulaz-basha-wal@users.noreply.github.com>
2024-09-11 18:14:16 +0530

    schema update, hook for pre-save to encrypt the password

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,15 +1,24 @@
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { IsEmail } from 'class-validator';
 import { USER_ROLES } from 'src/common/constants/user.constants';
+import { Document } from 'mongoose';
 
 @Schema({ timestamps: true })
-export class User {
+export class User extends Document {
   @Prop({ required: true })
-  name: string;
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
 
   @Prop({ required: true, unique: true })
   @IsEmail()
   email: string;
+
+  @Prop({ required: true, select: false })
+  password: string;
 
   @Prop({ required: true, default: USER_ROLES.USER })
   userRoleId: USER_ROLES;
@@ -25,6 +34,40 @@ export class User {
 
   @Prop({ default: () => new Date() })
   roleLastUpdatedAt: Date;
+
+  comparePassword(password: string) {
+    return bcrypt.compare(password, this.password);
+  }
+
+  generateAuthToken() {
+    const token = jwt.sign(
+      {
+        _id: this._id,
+        name: `${this.firstName} ${this.lastName}`,
+        roleId: this.userRoleId,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_EXPIRY || '1d',
+      },
+    );
+    return token;
+  }
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(this.password, salt);
+      this.password = hash;
+      next();
+    } catch (err) {
+      return next(err);
+    }
+  } else {
+    return next();
+  }
+});
